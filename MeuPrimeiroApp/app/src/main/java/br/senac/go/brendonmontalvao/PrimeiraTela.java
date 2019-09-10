a50449157444443f06e7b2ec7cd083179efd408a
brendonpm <brendon.pm@gmail.com>
2019-09-10 20:52:52 -0300

    projeto meu primeiro app

--- MeuPrimeiroApp/app/src/main/java/br/senac/go/brendonmontalvao/PrimeiraTela.java
+++ MeuPrimeiroApp/app/src/main/java/br/senac/go/brendonmontalvao/PrimeiraTela.java
@@ -3,10 +3,9 @@ package br.senac.go.brendonmontalvao;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class PrimeiraTela extends AppCompatActivity {
@@ -18,14 +17,27 @@ public class PrimeiraTela extends AppCompatActivity {
         setContentView(R.layout.activity_primeira_tela);
 
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        findViewById(R.id.editText).setEnabled(true);
+
 
     }
 
     public void tela2(View view){
 
+
         Intent intent = new Intent(this, CronogramaActivity.class);
 
         intent.putExtra("teste",R.id.textView4);
+
+        EditText text = findViewById(R.id.editText);
+        Toast.makeText(this, text.getText(), Toast.LENGTH_SHORT).show();
+
 
         startActivity(intent);
 
@@ -35,6 +47,12 @@ public class PrimeiraTela extends AppCompatActivity {
         Intent intent = new Intent(this, SobreActivity.class);
 
         intent.putExtra("teste",R.id.textView4);
+
+        EditText text = findViewById(R.id.editText);
+
+        text.setText("blablabla");
+
+        findViewById(R.id.editText).setEnabled(false);
 
         startActivity(intent);
 
